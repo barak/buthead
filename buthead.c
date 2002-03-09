ee76be3e2639f707d271dc9c2b871cfad9dc4c88
bap <bap>
2002-03-09 07:46:27 +0000

    detect overflown command line arg

--- buthead.c
+++ buthead.c
@@ -1,6 +1,6 @@
-// buthead: pass through all but the first n lines of standard input.
+// buthead: pass through all but first n lines of standard input
 
-// Copyright (C) 2001, Barak A. Pearlmutter <bap@cs.unm.edu>
+// Copyright (C) 2002, Barak A. Pearlmutter <bap@cs.unm.edu>
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -9,43 +9,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
-int main(argc,argv)
-     int argc;
-     char **argv;
+int main(int argc, char **argv)
 {
-  long lines_to_skip;
-  long i;
+  long lines_to_skip, i;
   int c;
   char *p;
 
-  if (argc != 2)
-    {
-      fprintf(stderr, "Usage: %s count.\n", argv[0]);
-      exit(2);
-    }
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s count.\n", argv[0]);
+    exit(2);
+  }
 
   lines_to_skip = strtol(argv[1], &p, 10);
 
-  if (lines_to_skip < 0 || argv[1][0] == '\0' || *p != '\0')
-    {
-      fprintf(stderr, "buthead error: invalid line count '%s'.\n",
-	      argv[1]);
-      exit(2);
+  if (errno || lines_to_skip < 0 || argv[1][0] == '\0' || *p != '\0') {
+    fprintf(stderr, "buthead error: invalid line count '%s'.\n", argv[1]);
+    exit(2);
+  }
+
+  for (i=0; i<lines_to_skip; ) {
+    if ((c = getc(stdin)) == EOF) {
+      fprintf(stderr, "buthead error: EOF on line %ld while skipping %ld.\n",
+	      i+1, lines_to_skip);
+      exit(1);
     }
 
-  for (i=0; i<lines_to_skip; )
-    {
-      if ((c = getc(stdin)) == EOF)
-	{
-	  fprintf(stderr,
-		  "buthead error: EOF on line %ld while skipping %ld.\n",
-		  i+1, lines_to_skip);
-	  exit(1);
-	}
-
-      if (c == '\n') i += 1;
-    }
+    if (c == '\n') i += 1;
+  }
 
   while ((c = getchar()) != EOF)
     putchar(c);
