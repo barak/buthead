19b0f350efed5e56ac0f469860b895670045ca01
barak <barak>
2003-09-28 04:36:04 +0000

    shorten

--- buthead.c
+++ buthead.c
@@ -1,6 +1,6 @@
 // buthead: pass through all but first n lines of standard input
 
-// Copyright (C) 2002, Barak A. Pearlmutter <bap@cs.unm.edu>
+// Copyright 2002, Barak A. Pearlmutter <bap@debian.org>
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,20 +18,20 @@ int main(int argc, char **argv)
   char *p;
 
   if (argc != 2) {
-    fprintf(stderr, "Usage: %s count.\n", argv[0]);
+    fprintf(stderr, "Usage: %s count\n", argv[0]);
     exit(2);
   }
 
   lines_to_skip = strtol(argv[1], &p, 10);
 
   if (errno || lines_to_skip < 0 || argv[1][0] == '\0' || *p != '\0') {
-    fprintf(stderr, "buthead error: invalid line count '%s'.\n", argv[1]);
+    fprintf(stderr, "buthead error: invalid line count '%s'\n", argv[1]);
     exit(2);
   }
 
   for (i=0; i<lines_to_skip; ) {
     if ((c = getc(stdin)) == EOF) {
-      fprintf(stderr, "buthead error: EOF on line %ld while skipping %ld.\n",
+      fprintf(stderr, "buthead error: EOF on line %ld while skipping %ld\n",
 	      i+1, lines_to_skip);
       exit(1);
     }
